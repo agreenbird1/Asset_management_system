e8433c4c0f788484143f0fae4c8529732c2c6579
afanti <afanti@juexiaotime.com>
2023-03-26 20:55:51 +0800

    引入naive ui

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles/index.less'
+import naive from 'naive-ui'
 import router from './router'
 
 const app = createApp(App)
-app.use(router)
+app.use(router).use(naive)
 app.mount('#app')

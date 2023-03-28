cdb344c398eed7a1f7dc44f92959fffe39d3e29e
afanti <afanti@juexiaotime.com>
2023-03-28 23:18:35 +0800

    table接入

--- src/views/MyHome/index.vue
+++ src/views/MyHome/index.vue
@@ -3,6 +3,25 @@
     <div class="projects-section">
       <div class="projects-section-header">
         <p>我的申请</p>
+        <div class="state">
+          <label>状态：</label>
+          <span
+            v-for="s in states"
+            :key="s.value"
+            :class="{ active: searchInfo.state == s.value }"
+            @click="searchInfo.state = s.value"
+            >{{ s.label }}</span
+          >
+        </div>
+      </div>
+      <div class="table">
+        <n-data-table
+          :columns="columns"
+          :data="data"
+          :pagination="pagination"
+          flex-height
+          style="height: 100%;"
+        />
       </div>
     </div>
     <ProfileSection />
@@ -10,35 +29,148 @@
 </template>
 
 <script setup lang="ts">
-import ProfileSection from './components/ProfileSection.vue';
+import { DataTableColumns, NButton, useMessage } from 'naive-ui'
+import { h, reactive, ref } from 'vue'
+import ProfileSection from './components/ProfileSection.vue'
+
+const message = useMessage()
+const searchInfo = ref({
+  pageNum: 1,
+  state: 1,
+})
+
+const states = [
+  {
+    label: '审批中',
+    value: 1,
+  },
+  {
+    label: '已批准',
+    value: 2,
+  },
+  {
+    label: '已驳回',
+    value: 3,
+  },
+  {
+    label: '已签收',
+    value: 4,
+  },
+]
+
+type Song = {
+  no: number
+  title: string
+  length: string
+}
+
+const createColumns = ({
+  play,
+}: {
+  play: (row: Song) => void
+}): DataTableColumns<Song> => {
+  return [
+    {
+      title: '资产/物品',
+      key: 'no',
+    },
+    {
+      title: '规格型号',
+      key: 'title',
+    },
+    {
+      title: '申请数量',
+      key: 'length',
+    },{
+      title: '申请日期',
+      key: 'date',
+    },{
+      title: '申请状态',
+      key: 'state',
+    }
+  ]
+}
+
+const data: Song[] = [
+  { no: 3, title: 'Wonderwall', length: '4:18' },
+  { no: 4, title: "Don't Look Back in Anger", length: '4:48' },
+  { no: 12, title: 'Champagne Supernova', length: '7:27' },
+  { no: 12, title: 'Champagne Supernova', length: '7:27' },
+  { no: 12, title: 'Champagne Supernova', length: '7:27' },
+  { no: 12, title: 'Champagne Supernova', length: '7:27' },
+  { no: 12, title: 'Champagne Supernova', length: '7:27' },
+  { no: 12, title: 'Champagne Supernova', length: '7:27' },
+  { no: 12, title: 'Champagne Supernova', length: '7:27' },
+]
+
+const columns = createColumns({
+  play(row: Song) {
+    message.info(`Play ${row.title}`)
+  },
+})
+const pagination = reactive({
+  page: 2,
+  pageSize: 5,
+  showSizePicker: true,
+  pageSizes: [5, 10, 20],
+  onChange: (page: number) => {
+    searchInfo.value.pageNum = page
+    pagination.page = page
+  },
+  onUpdatePageSize: (pageSize: number) => {
+    pagination.pageSize = pageSize
+    pagination.page = 1
+  },
+})
 </script>
 
 <style scoped lang="less">
 .projects-section {
   flex: 2;
-  background-color: var(--projects-section);
-  border-radius: 32px;
-  padding: 32px 32px 0 32px;
   overflow: hidden;
   height: 100%;
   display: flex;
   flex-direction: column;
-
+  justify-content: space-between;
   &-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
     margin-bottom: 24px;
     color: var(--main-color);
-
+    border-radius: 15px;
+    padding: 10px 20px;
+    background-color: var(--projects-section);
     p {
       font-size: 24px;
       line-height: 32px;
       font-weight: 700;
       opacity: 0.9;
       margin: 0;
+      margin-bottom: 20px;
       color: var(--main-color);
     }
+    .state {
+      label {
+        margin-right: 50px;
+      }
+      span {
+        font-size: 16px;
+        font-weight: 300;
+        border-radius: 20px;
+        padding: 4px 20px;
+        display: inline-block;
+        cursor: pointer;
+        margin-right: 20px;
+      }
+      .active {
+        background: var(--link-color-hover);
+        color: #ffffff;
+      }
+    }
+  }
+  .table {
+    padding: 10px 20px;
+    flex: 1;
+    background-color: #fff;
+    border-radius: 15px;
   }
 }
 

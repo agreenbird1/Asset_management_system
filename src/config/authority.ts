62315b910288db208da9530f945b1e9e00a0bf52
afanti <afanti@juexiaotime.com>
2023-03-28 22:05:49 +0800

    侧边栏权限控制

---
+++ src/config/authority.ts
@@ -0,0 +1,52 @@
+import {
+  HomeOutline,
+  AlbumsOutline,
+  BarcodeOutline,
+  ColorFilterOutline,
+  RecordingOutline,
+  EllipsisVerticalCircleOutline,
+  AppsOutline
+} from '@vicons/ionicons5'
+
+export const asideAuthority = [
+  {
+    index: 1,
+    label: '我的主页',
+    icon: HomeOutline,
+    route:'/my-home'
+  },
+  {
+    index: 1,
+    label: '我的资产',
+    icon: AlbumsOutline,
+    route:'/my-home'
+  },
+  {
+    index: 2,
+    label: '资产分类',
+    icon: BarcodeOutline,
+    route:'/my-home'
+  },
+  {
+    index: 2,
+    label: '企业成员',
+    icon: ColorFilterOutline,
+    route:'/my-home'
+  },
+  {
+    index: 2,
+    label: '资产监控',
+    icon: RecordingOutline,
+    route:'/my-home'
+  },{
+    index: 2,
+    label: '企业资产管理',
+    icon: EllipsisVerticalCircleOutline,
+    route:'/my-home'
+  },{
+    index: 3,
+    label: '企业管理',
+    icon: AppsOutline,
+    route:'/my-home'
+  }
+]

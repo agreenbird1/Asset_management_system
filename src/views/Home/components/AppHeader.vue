40de029a7a3480d667c5dc4aa05a45f6af7bf42d
afanti <afanti@juexiaotime.com>
2023-03-28 21:07:30 +0800

    header拆分

---
+++ src/views/Home/components/AppHeader.vue
@@ -0,0 +1,123 @@
+<template>
+  <header class="app-header">
+    <div class="app-header-left">
+      <img src="@/assets/favicon.ico" alt="">
+      <p class="app-name">固定资产管理</p>
+      <div class="search-wrapper">
+        <input class="search-input" type="text" placeholder="搜索当前内容" />
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
+          stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="feather feather-search"
+          viewBox="0 0 24 24">
+          <defs></defs>
+          <circle cx="11" cy="11" r="8"></circle>
+          <path d="M21 21l-4.35-4.35"></path>
+        </svg>
+      </div>
+    </div>
+    <div class="app-header-right">
+      <button class="mode-switch" title="Switch Theme" @click="changeSkin">
+        <svg class="moon" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+          stroke-width="2" width="24" height="24" viewBox="0 0 24 24">
+          <defs></defs>
+          <path d="M21 12.79A9 9 0 1111.21 3 7 7 0 0021 12.79z"></path>
+        </svg>
+      </button>
+      <NotificationButton />
+      <span class="time ml-20">December，26</span>
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import NotificationButton from './NotificationButton.vue';
+
+const changeSkin = () => {
+  var modeSwitch = document.querySelector('.mode-switch')!;
+  document.documentElement.classList.toggle('dark');
+  modeSwitch.classList.toggle('active');
+}
+</script>
+
+<style scoped lang="less">
+.app {
+
+  &-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    padding: 16px 24px;
+    position: relative;
+
+    &-left,
+    &-right {
+      display: flex;
+      align-items: center;
+    }
+
+    &-left {
+      flex-grow: 1;
+    }
+
+    &-right button {
+      margin-left: 10px;
+    }
+  }
+  &-name {
+    color: var(--main-color);
+    margin: 0;
+    font-size: 20px;
+    line-height: 24px;
+    font-weight: 700;
+    margin: 0 32px;
+  }
+}
+
+.mode-switch {
+  background-color: transparent;
+  border: none;
+  margin-right: 10px;
+  padding: 0;
+  color: var(--main-color);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.search-wrapper {
+  border-radius: 20px;
+  background-color: var(--search-area-bg);
+  padding-right: 12px;
+  height: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  max-width: 480px;
+  color: var(--light-font);
+  box-shadow: 0 2px 6px 0 rgba(136, 148, 171, 0.2),
+    0 24px 20px -24px rgba(71, 82, 107, 0.1);
+  overflow: hidden;
+
+  .dark & {
+    box-shadow: none;
+  }
+}
+
+.search-input {
+  border: none;
+  flex: 1;
+  outline: none;
+  height: 100%;
+  padding: 0 20px;
+  font-size: 16px;
+  background-color: var(--search-area-bg);
+  color: var(--main-color);
+
+  &:placeholder {
+    color: var(--main-color);
+    opacity: 0.6;
+  }
+}
+
+</style>

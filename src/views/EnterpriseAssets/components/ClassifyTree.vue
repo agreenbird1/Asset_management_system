fd4669857eb11cad6a5baa5db6082f6645ed805e
afanti <afanti@juexiaotime.com>
2023-04-06 23:19:10 +0800

    feat: UI更新

---
+++ src/views/EnterpriseAssets/components/ClassifyTree.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="classify-tree">
+    <n-tree
+      block-line
+      :data="treeData"
+      :selectable="false"
+      :render-suffix="suffix"
+    />
+    <div class="df aic jcc mt-20">
+      <n-button color="#6a83d0" @click="showAddDialog = true"
+        >添加分类</n-button
+      >
+    </div>
+    <Dialog v-model="showAddDialog" title="添加分类"></Dialog>
+    <Dialog v-model="showUpdateDialog" title="修改分类"></Dialog>
+    <Dialog v-model="showDeleteDialog" title="删除分类"></Dialog>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { NButton, TreeOption } from 'naive-ui'
+import { ref, h } from 'vue'
+import Dialog from '@/components/Dialog/index.vue'
+
+const showAddDialog = ref(false)
+const showUpdateDialog = ref(false)
+const showDeleteDialog = ref(false)
+
+const treeData = ref([
+  {
+    label: '1',
+    key: 1,
+    children: [
+      {
+        label: '1-1',
+        key: 2,
+        level: 2,
+      },
+      {
+        label: '1-2',
+        key: 3,
+        level: 2,
+      },
+      {
+        label: '1-3',
+        key: 4,
+        level: 2,
+        children: [
+          {
+            label: '1-3-1',
+            key: 5,
+            level: 3,
+          },
+        ],
+      },
+    ],
+    level: 1,
+  },
+  {
+    label: '2',
+    key: 6,
+    level: 1,
+  },
+  {
+    label: '3',
+    key: 7,
+    level: 1,
+  },
+  {
+    label: '4',
+    key: 8,
+    level: 1,
+  },
+])
+const suffix = (option: TreeOption) => [
+  h(
+    NButton,
+    {
+      text: true,
+      type: 'primary',
+      color: '#6a83d0',
+      onClick: () => (showAddDialog.value = true),
+    },
+    { default: () => '添加下级' }
+  ),
+  h(
+    NButton,
+    {
+      text: true,
+      type: 'warning',
+      class: 'ml-10',
+      onClick: () => (showUpdateDialog.value = true),
+    },
+    { default: () => '修改' }
+  ),
+  h(
+    NButton,
+    {
+      text: true,
+      type: 'error',
+      class: 'ml-10',
+      onClick: () => (showDeleteDialog.value = true),
+    },
+    { default: () => '删除' }
+  ),
+]
+</script>
+
+<style scoped lang="less">
+.classify-tree {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  border-radius: 15px;
+  padding: 15px;
+  header {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+}
+</style>
--- src/views/EnterpriseAssets/index.vue
+++ src/views/EnterpriseAssets/index.vue
@@ -1,7 +1,53 @@
 <template>
-  <div class="enterprise-assets">enterprise-assets</div>
+  <main class="enterprise-assets">
+    <ClassifyTree />
+    <n-data-table
+      class="table"
+      :columns="columns"
+      :data="data"
+      :pagination="pagination"
+      :bordered="false"
+    />
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import ClassifyTree from './components/ClassifyTree.vue'
 
-<style scoped lang="less"></style>
+const columns = ref([
+  {
+    title: 'No',
+    key: 'no',
+  },
+  {
+    title: 'Title',
+    key: 'title',
+  },
+])
+const data = ref([])
+const pagination = ref([])
+
+const searchInfo = ref({
+  name: '',
+  phone: '',
+})
+</script>
+
+<style scoped lang="less">
+.enterprise-assets {
+  flex: 1;
+  display: flex;
+  height: 100%;
+  .classify-tree {
+    flex: 30%;
+    margin-right: 20px;
+  }
+  .n-data-table {
+    border-radius: 15px;
+    overflow: hidden;
+    background-color: #fff;
+    padding: 10px;
+  }
+}
+</style>

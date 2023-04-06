93ce54ff2d94baba2222f34a62dc415aa43e9d0c
afanti <afanti@juexiaotime.com>
2023-04-06 23:13:22 +0800

    企业成员管理

---
+++ src/views/EnterpriseMember/components/ClassifyTree.vue
@@ -0,0 +1,80 @@
+<template>
+  <aside class="classify-tree">
+    <n-tree block-line :data="treeData" selectable />
+    <div class="button-wrapper mt-30">
+      <span class="add-button"> +添加部门分类 </span>
+    </div>
+  </aside>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const treeData = ref([
+  {
+    label: '1',
+    key: 1,
+    children: [
+      {
+        label: '1-1',
+        key: 2,
+        level: 2,
+      },
+      {
+        label: '1-2',
+        key: 3,
+        level: 2,
+      },
+      {
+        label: '1-3',
+        key: 4,
+        level: 2,
+        children: [
+          {
+            label: '1-3-1',
+            key: 5,
+            level: 3,
+          },
+        ],
+      },
+    ],
+    level: 1,
+  },
+  {
+    label: '2',
+    key: 6,
+    level: 1,
+  },
+  {
+    label: '3',
+    key: 7,
+    level: 1,
+  },
+  {
+    label: '4',
+    key: 8,
+    level: 1,
+  },
+])
+</script>
+
+<style scoped lang="less">
+.classify-tree {
+  background-color: #fff;
+  border-radius: 15px;
+  padding: 10px;
+  height: 100%;
+  overflow: auto;
+
+  .button-wrapper {
+    text-align: center;
+  }
+  .add-button {
+    padding: 5px 10px;
+    border: 2px solid var(--main-color);
+    border-radius: 5px;
+    font-size: 14px;
+    cursor: pointer;
+  }
+}
+</style>
--- src/views/EnterpriseMember/index.vue
+++ src/views/EnterpriseMember/index.vue
@@ -1,7 +1,89 @@
 <template>
-  <div class="enterprise-member">enterprise-member</div>
+  <div class="enterprise-member">
+    <div class="search-header">
+      <n-input
+        v-model:value="searchInfo.name"
+        type="text"
+        placeholder="员工名字"
+        size="small"
+        autocomplete="none"
+      />
+      <n-input
+        v-model:value="searchInfo.phone"
+        type="text"
+        placeholder="员工手机"
+        size="small"
+        aria-autocomplete="none"
+      />
+      <n-button color="#6a83d0" size="small"> 查询 </n-button>
+    </div>
+    <main>
+      <ClassifyTree />
+      <n-data-table
+        class="table"
+        :columns="columns"
+        :data="data"
+        :pagination="pagination"
+        :bordered="false"
+      />
+    </main>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import ClassifyTree from './components/ClassifyTree.vue'
 
-<style scoped lang="less"></style>
+const columns = ref([
+  {
+    title: 'No',
+    key: 'no',
+  },
+  {
+    title: 'Title',
+    key: 'title',
+  },
+])
+const data = ref([])
+const pagination = ref([])
+
+const searchInfo = ref({
+  name: '',
+  phone: '',
+})
+</script>
+
+<style scoped lang="less">
+.enterprise-member {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+
+  .search-header {
+    padding: 10px;
+    background-color: #fff;
+    border-radius: 15px;
+    margin-bottom: 20px;
+    .n-input {
+      display: inline-block;
+      width: unset;
+      margin-right: 10px;
+    }
+  }
+
+  main {
+    flex: 1;
+    display: flex;
+    .classify-tree {
+      flex: 25%;
+      margin-right: 20px;
+    }
+    .n-data-table {
+      border-radius: 15px;
+      overflow: hidden;
+      background-color: #fff;
+      padding: 10px;
+    }
+  }
+}
+</style>

741e93e30fa228a550c2a04eda721fe0fa2072b0
afanti <afanti@juexiaotime.com>
2023-04-08 23:41:35 +0800

    feat：消息通知样式

--- src/views/Home/components/NotificationButton.vue
+++ src/views/Home/components/NotificationButton.vue
@@ -1,26 +1,46 @@
 <template>
-  <n-badge :value="active" dot :offset="[-2, 8]">
+  <n-badge :value="1" dot :offset="[-2, 8]">
     <button class="notification-btn" @click="active = true">
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
-        stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-bell">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-bell"
+      >
         <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
         <path d="M13.73 21a2 2 0 0 1-3.46 0" />
       </svg>
     </button>
   </n-badge>
 
-  <n-drawer v-model:show="active" width="25%" placement="right">
-    <n-drawer-content title="斯通纳">
-      《斯通纳》是美国作家约翰·威廉姆斯在 1965 年出版的小说。
-    </n-drawer-content>
+  <n-drawer v-model:show="active" width="25%" class="p-10" placement="right">
+    <div class="df mb-10" style="flex-direction: row-reverse">
+      <n-button color="#6a83d0">已读所有信息</n-button>
+    </div>
+    <n-card hoverable>
+      <div class="df jcsb aic">
+        您申请的小米lite鼠标已经成功签收！
+        <n-button color="#6a83d0" quaternary>标为已读</n-button>
+      </div>
+    </n-card>
   </n-drawer>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-import { NDrawer } from 'naive-ui';
+import { ref } from 'vue'
+import { NDrawer } from 'naive-ui'
 
 const active = ref(false)
+
+const closeMessage = () => {
+  console.log('closeMessage')
+}
 </script>
 
 <style scoped lang="less">
@@ -33,5 +53,9 @@ const active = ref(false)
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.message-status {
+  color: var(--main-color);
 }
 </style>

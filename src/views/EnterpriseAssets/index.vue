976ffa47c503fdaf17d6c9cf8cd096e2e6f7fc15
afanti <afanti@juexiaotime.com>
2023-04-08 16:13:03 +0800

    feat: 资产页UI

--- src/views/EnterpriseAssets/index.vue
+++ src/views/EnterpriseAssets/index.vue
@@ -1,14 +1,33 @@
 <template>
-  <main class="enterprise-assets">
-    <ClassifyTree />
-    <n-data-table
-      class="table"
-      :columns="columns"
-      :data="data"
-      :pagination="pagination"
-      :bordered="false"
-    />
-  </main>
+  <div class="enterprise-assets">
+    <div class="search-header">
+      <n-input
+        v-model:value="searchInfo.name"
+        type="text"
+        placeholder="员工名字"
+        size="small"
+        autocomplete="none"
+      />
+      <n-input
+        v-model:value="searchInfo.phone"
+        type="text"
+        placeholder="员工手机"
+        size="small"
+        aria-autocomplete="none"
+      />
+      <n-button color="#6a83d0" size="small"> 查询 </n-button>
+    </div>
+    <main>
+      <ClassifyTree />
+      <n-data-table
+        class="table"
+        :columns="columns"
+        :data="data"
+        :pagination="pagination"
+        :bordered="false"
+      />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -36,18 +55,34 @@ const searchInfo = ref({
 
 <style scoped lang="less">
 .enterprise-assets {
-  flex: 1;
-  display: flex;
   height: 100%;
-  .classify-tree {
-    flex: 30%;
-    margin-right: 20px;
-  }
-  .n-data-table {
-    border-radius: 15px;
-    overflow: hidden;
-    background-color: #fff;
+  .search-header {
     padding: 10px;
+    background-color: #fff;
+    border-radius: 15px;
+    margin-bottom: 20px;
+    .n-input {
+      display: inline-block;
+      width: unset;
+      margin-right: 10px;
+    }
+  }
+
+  main {
+    flex: 1;
+    display: flex;
+    height: 100%;
+
+    .classify-tree {
+      flex: 30%;
+      margin-right: 20px;
+    }
+    .n-data-table {
+      border-radius: 15px;
+      overflow: hidden;
+      background-color: #fff;
+      padding: 10px;
+    }
   }
 }
 </style>

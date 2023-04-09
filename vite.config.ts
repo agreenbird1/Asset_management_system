b60d041f1302a5cb7234bac291bb620b77e5a65e
afanti <afanti@juexiaotime.com>
2023-04-09 20:27:48 +0800

    chore: 代理

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "http://localhost:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },

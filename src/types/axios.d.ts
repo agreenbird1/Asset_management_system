262220c99561adfb4efebfeab4db4f923e86643f
afanti <afanti@juexiaotime.com>
2023-04-19 23:55:59 +0800

    返回接口定义总定义

--- src/types/axios.d.ts
+++ src/types/axios.d.ts
@@ -1,7 +1,15 @@
-import type { Method, AxiosPromise, AxiosRequestConfig } from "axios";
+import type { Method, AxiosPromise, AxiosRequestConfig } from 'axios'
 
 export interface AFanTiAxiosRequestConfig extends AxiosRequestConfig {
-  isLoading?: boolean;
+  isLoading?: boolean
+}
+
+export type ResponseData<T = any> = {
+  data: T
+  extra: any
+  message: string
+  status: number
+  success: boolean
 }
 
 // 总方法的声明
@@ -10,16 +18,16 @@ export type requestType = <T = any>(
   method: Method,
   data?: unknown,
   config?: AFanTiAxiosRequestConfig // 配置其他，包括 isLoading
-) => AxiosPromise<T>;
+) => Promise<ResponseData<T>>
 
 // 单独方法的声明
 export type requestMethodType = <T = any>(
   url: string,
   data?: unknown,
   config?: AFanTiAxiosRequestConfig // 配置其他，包括 isLoading
-) => AxiosPromise<T>;
+) => Promise<ResponseData<T>>
 
 // 直接生成每一个对应的简单的方法
 export type requestMethod = {
-  [k in "get" | "post" | "delete" | "patch"]: requestMethodType;
-};
+  [k in 'get' | 'post' | 'delete' | 'patch']: requestMethodType
+}

0b348920806329f73af6593ce02859795efa5d80
afanti <afanti@juexiaotime.com>
2023-04-20 23:12:51 +0800

    feat: 注册user store

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import App from './App.vue'
 import './assets/styles/index.less'
 import naive from 'naive-ui'
 import router from './router'
+import store from './store'
 
 const app = createApp(App)
-app.use(router).use(naive)
+app.use(router).use(naive).use(store)
 app.mount('#app')
--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,18 +1,21 @@
+import { IUser } from './../api/user'
 import { AsideAuthorityItem } from '@/config/authority'
+import storage from '@/utils/storage'
 import { defineStore } from 'pinia'
 
 type UserStoreState = {
-  userId?: number
-  asidePermission: AsideAuthorityItem[]
+  userInfo?: IUser
+  permission: AsideAuthorityItem[]
 }
 
 // useStore 可以是 useUser、useCart 之类的任何东西
 // 第一个参数是应用程序中 store 的唯一 id
 export const useUserStore = defineStore('userStore', {
   state: (): UserStoreState => {
+    const state = storage.getSession<UserStoreState>('userState')
     return {
-      userId: undefined,
-      asidePermission: [],
+      userInfo: state?.userInfo,
+      permission: state?.permission || [],
     }
   },
 })

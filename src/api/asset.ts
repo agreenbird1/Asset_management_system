9ae5db7957206e7ef1eda259978c6771d80e08d9
afanti <afanti@juexiaotime.com>
2023-05-01 11:26:49 +0800

    feat：资产详情

--- src/api/asset.ts
+++ src/api/asset.ts
@@ -18,6 +18,14 @@ export type IAsset = {
   serialNumber?: string
   status: 0 | 1
   category: ICategory
+  surplusQuantity: number
+  useTimes: number
+  maintenanceNumber: number
+  scrapNumber: number
+  maintenanceValue: number
+  scrapValue: number
+  depreciationValue: number
+  currentValue: number
 }
 
 export interface IAssetSearch extends PageInfo {
